fb4ca0a31023a6ab422796210caeed0e70b953f0
MonforteAdrian <adryanmonforte94@gmail.com>
2022-07-26 17:56:13 +0200

    Alacritty new main

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -68,7 +68,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Run: ", NULL };
-static const char *termcmd[]  = { "st", NULL };
+//static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *chromecmd[]  = { "google-chrome-stable", NULL };
 
 static Key keys[] = {
